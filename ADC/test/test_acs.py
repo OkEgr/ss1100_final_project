44e45455f1ec2644cb07e3da77d6bc3ab51e59e8
Raphael-88 <raphael.lopesborges.br@nps.edu>
2025-12-04 12:37:02 -0800

    ADC files change
    All the changes were done after resync

--- ADC/test/test_acs.py
+++ ADC/test/test_acs.py
@@ -2,9 +2,12 @@ import unittest
 import sys
 import os
 
+# This line adds the parent folder to the system path so Python can find your scripts
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from your_adc_script import calculate_rotation
+# UPDATE: Import from the new module (adc_utils) instead of the main script
+from adc_utils import calculate_rotation
+
 
 class TestACS(unittest.TestCase):
 
@@ -14,14 +17,15 @@ class TestACS(unittest.TestCase):
         """
         current = (30, 60, 90)
         target = (0, 10, 15)
-        
+
         # Expected correction = target - current
         expected_correction = (-30, -50, -75)
-        
+
         calculated_correction = calculate_rotation(current, target)
-        
+
         self.assertEqual(calculated_correction, expected_correction,
                          "Calculate rotation function is incorrect.")
+
 
 if __name__ == '__main__':
     unittest.main()
